a99013104ba93b912e4a0b1d573cb205b338e978
Rithvik Gampa <gamparithvik692@gmail.com>
2026-05-28 18:32:38 +0530

    Fix production CORS

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://zero2-one-git-main-rithvik-gampa-s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

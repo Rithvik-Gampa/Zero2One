207ebc35ad547f325e54b5303299043546371c8e
Rithvik Gampa <gamparithvik692@gmail.com>
2026-05-28 18:23:32 +0530

    Fix CORS for frontend deployment

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,15 @@ import requests
 # =====================================================
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # =====================================================
 # ================= CORS ==============================
 # =====================================================
